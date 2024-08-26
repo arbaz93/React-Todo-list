43d40b1b7b97613fe8658c3f6257e82969e3ca29
Arbaz Yousaf <arbaz.k480@yahoo.com>
2024-08-26 02:30:24 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/React-Todo-list/",
+  base:"/React-Todo-list",
   plugins: [react()]
 })
